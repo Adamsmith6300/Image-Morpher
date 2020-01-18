738b655676468504edc6a6aea9ae25f8672598fa
Adam <Adamsmith6300@gmail.com>
2020-01-18 13:17:49 -0800

    tried to get delete working, status=incomplete, moving on to start morph algo

--- app/src/main/java/com/example/circleviewdrawing/CirclesDrawingView.java
+++ app/src/main/java/com/example/circleviewdrawing/CirclesDrawingView.java
@@ -12,9 +12,12 @@ import android.util.Log;
 import android.util.SparseArray;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -46,22 +49,9 @@ public class CirclesDrawingView extends View {
         }
     }
 
-    /** Stores groups of circles to form lines **/
-//    public class CircleGroup<CircleArea,CircleArea> {
-//        private CircleArea l;
-//        private R r;
-//        public Pair(L l, R r){
-//            this.l = l;
-//            this.r = r;
-//        }
-//        public L getL(){ return l; }
-//        public R getR(){ return r; }
-//        public void setL(L l){ this.l = l; }
-//        public void setR(R r){ this.r = r; }
-//    }
-
     /** Paint to draw circles */
     private Paint mCirclePaint;
+    private Paint selectedMCirclePaint;
 
     private final Random mRadiusGenerator = new Random();
     // Radius limit in pixels
@@ -73,6 +63,8 @@ public class CirclesDrawingView extends View {
     private ArrayList<ArrayList<CircleArea>> mCircles = new ArrayList<ArrayList<CircleArea>>();
     private SparseArray<CircleArea> mCirclePointer = new SparseArray<CircleArea>();
 
+    private CircleArea lastTouched = new CircleArea(0,0,0);
+
     /**
      * Default constructor
      *
@@ -80,49 +72,111 @@ public class CirclesDrawingView extends View {
      */
     public CirclesDrawingView(final Context ct) {
         super(ct);
-
         init(ct);
     }
 
     public CirclesDrawingView(final Context ct, final AttributeSet attrs) {
         super(ct, attrs);
-
         init(ct);
     }
 
     public CirclesDrawingView(final Context ct, final AttributeSet attrs, final int defStyle) {
         super(ct, attrs, defStyle);
-
         init(ct);
     }
 
     private void init(final Context ct) {
         // Generate bitmap used for background
+        setFocusableInTouchMode(true);
         mBitmap = BitmapFactory.decodeResource(ct.getResources(), R.drawable.cat);
-        //mBitmap2 = BitmapFactory.decodeResource(ct.getResources(), R.drawable.cat);
 
         mCirclePaint = new Paint();
-
         mCirclePaint.setColor(Color.GREEN);
         mCirclePaint.setStrokeWidth(2);
         mCirclePaint.setStyle(Paint.Style.FILL);
+
+        selectedMCirclePaint = new Paint();
+        selectedMCirclePaint.setColor(Color.BLUE);
+        selectedMCirclePaint.setStrokeWidth(2);
+        selectedMCirclePaint.setStyle(Paint.Style.FILL);
     }
+
 
     @Override
     public void onDraw(final Canvas canv) {
         // background bitmap to cover all area
         canv.drawBitmap(mBitmap, null, mMeasuredRect, null);
-        //canv.drawBitmap(mBitmap2, null, mMeasuredRect, null);
-
-        for (List<CircleArea> circleList : mCircles){
+        for (ArrayList<CircleArea> circleList : mCircles){
             CircleArea c1 = circleList.get(0);
-            canv.drawCircle(c1.centerX, c1.centerY, c1.radius, mCirclePaint);
-            if(circleList.size() == 2){
+            if(circleList.size() == 1){
+                canv.drawCircle(c1.centerX, c1.centerY, c1.radius, c1.centerX == lastTouched.centerX ? selectedMCirclePaint : mCirclePaint);
+            } else {
                 CircleArea c2 = circleList.get(1);
-                canv.drawCircle(c2.centerX, c2.centerY, c2.radius, mCirclePaint);
-                canv.drawLine(c1.centerX, c1.centerY, c2.centerX, c2.centerY, mCirclePaint);
+                if(c1.centerX == lastTouched.centerX || c2.centerX == lastTouched.centerX){
+                    canv.drawCircle(c1.centerX, c1.centerY, c1.radius, selectedMCirclePaint);
+                    canv.drawCircle(c2.centerX, c2.centerY, c2.radius, selectedMCirclePaint);
+                    canv.drawLine(c1.centerX, c1.centerY, c2.centerX, c2.centerY, selectedMCirclePaint);
+                } else{
+                    canv.drawCircle(c1.centerX, c1.centerY, c1.radius, mCirclePaint);
+                    canv.drawCircle(c2.centerX, c2.centerY, c2.radius, mCirclePaint);
+                    canv.drawLine(c1.centerX, c1.centerY, c2.centerX, c2.centerY, mCirclePaint);
+                }
             }
         }
+    }
+
+    private View findViewAtPosition(View parent, int x, int y) {
+        if (parent instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup)parent;
+            for (int i=0; i<viewGroup.getChildCount(); i++) {
+                View child = viewGroup.getChildAt(i);
+                View viewAtPosition = findViewAtPosition(child, x, y);
+                if (viewAtPosition != null) {
+                    return viewAtPosition;
+                }
+            }
+            return null;
+        } else {
+            Rect rect = new Rect();
+            parent.getGlobalVisibleRect(rect);
+            if (rect.contains(x, y)) {
+                return parent;
+            } else {
+                return null;
+            }
+        }
+    }
+
+    public void resetLastTouched(){
+        lastTouched = new CircleArea(0,0,0);
+    }
+
+    public void deleteLastTouched(){
+        Iterator<ArrayList<CircleArea>> i = mCircles.iterator();
+        while (i.hasNext()) {
+            List<CircleArea> circleList = i.next(); // must be called before you can call i.remove()
+            CircleArea c1 = circleList.get(0);
+            if(circleList.size() == 1 && c1.centerX == lastTouched.centerX){
+                i.remove();
+            } else {
+                CircleArea c2 = circleList.get(1);
+                if(c1.centerX == lastTouched.centerX || c2.centerX == lastTouched.centerX){
+                    i.remove();
+                }
+            }
+        }
+
+//        for (List<CircleArea> circleList : mCircles){
+//            CircleArea c1 = circleList.get(0);
+//            if(circleList.size() == 1 && c1.centerX == lastTouched.centerX){
+//
+//            } else {
+//                CircleArea c2 = circleList.get(1);
+//                if(c1.centerX == lastTouched.centerX || c2.centerX == lastTouched.centerX){
+//
+//                }
+//            }
+//        }
     }
 
     @Override
@@ -145,17 +199,18 @@ public class CirclesDrawingView extends View {
                 yTouch = (int) event.getY(0);
 
                 // check if we've touched inside some circle
-                touchedCircle = obtainTouchedCircle(xTouch, yTouch);
+                touchedCircle = obtainTouchedCircle(xTouch, yTouch, event);
                 touchedCircle.centerX = xTouch;
                 touchedCircle.centerY = yTouch;
                 mCirclePointer.put(event.getPointerId(0), touchedCircle);
+                lastTouched = touchedCircle;
 
                 invalidate();
                 handled = true;
                 break;
 
             case MotionEvent.ACTION_POINTER_DOWN:
-                Log.w(TAG, "Pointer down");
+                //Log.w(TAG, "Pointer down");
                 // It secondary pointers, so obtain their ids and check circles
                 pointerId = event.getPointerId(actionIndex);
 
@@ -163,11 +218,13 @@ public class CirclesDrawingView extends View {
                 yTouch = (int) event.getY(actionIndex);
 
                 // check if we've touched inside some circle
-                touchedCircle = obtainTouchedCircle(xTouch, yTouch);
+                touchedCircle = obtainTouchedCircle(xTouch, yTouch, event);
 
                 mCirclePointer.put(pointerId, touchedCircle);
                 touchedCircle.centerX = xTouch;
                 touchedCircle.centerY = yTouch;
+                lastTouched = touchedCircle;
+
                 invalidate();
                 handled = true;
                 break;
@@ -175,8 +232,7 @@ public class CirclesDrawingView extends View {
             case MotionEvent.ACTION_MOVE:
                 final int pointerCount = event.getPointerCount();
 
-                Log.w(TAG, "Move");
-
+                //Log.w(TAG, "Move");
                 for (actionIndex = 0; actionIndex < pointerCount; actionIndex++) {
                     // Some pointer has moved, search it by pointer id
                     pointerId = event.getPointerId(actionIndex);
@@ -189,8 +245,10 @@ public class CirclesDrawingView extends View {
                     if (null != touchedCircle) {
                         touchedCircle.centerX = xTouch;
                         touchedCircle.centerY = yTouch;
+                        lastTouched = touchedCircle;
                     }
                 }
+
                 invalidate();
                 handled = true;
                 break;
@@ -226,8 +284,7 @@ public class CirclesDrawingView extends View {
      * Clears all CircleArea - pointer id relations
      */
     private void clearCirclePointer() {
-        Log.w(TAG, "clearCirclePointer");
-
+        //Log.w(TAG, "clearCirclePointer");
         mCirclePointer.clear();
     }
 
@@ -239,29 +296,47 @@ public class CirclesDrawingView extends View {
      *
      * @return obtained {@link CircleArea}
      */
-    private CircleArea obtainTouchedCircle(final int xTouch, final int yTouch) {
+    private CircleArea obtainTouchedCircle(final int xTouch, final int yTouch, final MotionEvent event) {
         CircleArea touchedCircle = getTouchedCircle(xTouch, yTouch);
-
         if (null == touchedCircle) {
-            touchedCircle = new CircleArea(xTouch, yTouch, RADIUS_LIMIT);
-
-            Log.w(TAG, "Added circle " + touchedCircle);
-            if (mCircles.size() > 0){
-                ArrayList<CircleArea> latestList = mCircles.get(mCircles.size() - 1);
-                if (latestList.size() < 2) {
-                    latestList.add(touchedCircle);
+                //Log.i(TAG, "View under finger: " + findViewAtPosition(getRootView(), (int)event.getRawX(), (int)event.getRawY()));
+                View currentCanvas = findViewAtPosition(getRootView(), (int)event.getRawX(), (int)event.getRawY());
+                //Log.i("ID",getResources().getResourceEntryName(currentCanvas.getId())+"");
+                String currentCanvasName = getResources().getResourceEntryName(currentCanvas.getId());
+                //Log.w("equal?",currentCanvasName.equalsIgnoreCase("firstImage")+"");
+                if(currentCanvasName.equalsIgnoreCase("firstImage")){
+                    CirclesDrawingView secondImg = getRootView().findViewById(R.id.secondImage);
+                    //Log.w("Grabbed view", secondImg.toString());
+                    CircleArea circleCopy = secondImg.createCircleCopy(xTouch, yTouch);
+                    secondImg.invalidate();
                 } else {
-                    ArrayList<CircleArea> newList = new ArrayList<CircleArea>();
-                    newList.add(touchedCircle);
-                    mCircles.add(newList);
+                    CirclesDrawingView firstImg = getRootView().findViewById(R.id.firstImage);
+                    CircleArea circleCopy = firstImg.createCircleCopy(xTouch, yTouch);
+                    firstImg.invalidate();
                 }
+            touchedCircle = createCircleCopy(xTouch, yTouch);
+        }
+
+        return touchedCircle;
+    }
+
+    public CircleArea createCircleCopy(final int xTouch, final int yTouch){
+        CircleArea touchedCircle = new CircleArea(xTouch, yTouch, RADIUS_LIMIT);
+        //Log.w(TAG, "Added circle " + touchedCircle);
+        if (mCircles.size() > 0){
+            ArrayList<CircleArea> latestList = mCircles.get(mCircles.size() - 1);
+            if (latestList.size() < 2) {
+                latestList.add(touchedCircle);
             } else {
                 ArrayList<CircleArea> newList = new ArrayList<CircleArea>();
                 newList.add(touchedCircle);
                 mCircles.add(newList);
             }
+        } else {
+            ArrayList<CircleArea> newList = new ArrayList<CircleArea>();
+            newList.add(touchedCircle);
+            mCircles.add(newList);
         }
-
         return touchedCircle;
     }
 
@@ -297,12 +372,11 @@ public class CirclesDrawingView extends View {
     @Override
     protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
         mMeasuredRect = new Rect(0, 0, getMeasuredWidth(), getMeasuredHeight());
     }
 
     public void clearLines(){
-        Log.w(TAG, "Clear all circles, size is " + mCircles.size());
+        //Log.w(TAG, "Clear all circles, size is " + mCircles.size());
         // remove first circle
         mCircles.clear();
         invalidate();
--- app/src/main/java/com/example/circleviewdrawing/MainActivity.java
+++ app/src/main/java/com/example/circleviewdrawing/MainActivity.java
@@ -4,22 +4,57 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CirclesDrawingView firstImg;
+    private CirclesDrawingView secondImg;
+
+    private int numberOfFrames = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        firstImg = findViewById(R.id.firstImage);
+        secondImg = findViewById(R.id.secondImage);
+
+//        firstImg.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//            @Override
+//            public void onFocusChange(View v, boolean hasFocus) {
+//                    Log.i("CHANGEFOCUS",v.toString());
+//                if(!hasFocus){
+//
+//                }
+//            }
+//        });
+//        secondImg.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//            @Override
+//            public void onFocusChange(View v, boolean hasFocus) {
+//                    Log.i("CHANGEFOCUS",v.toString());
+//                if(!hasFocus){
+//                  //  v.resetLastTouched();
+//                }
+//            }
+//        });
+
     }
 
+//    public void deleteLine(android.view.View v){
+//        (CirclesDrawingView) v.deleteLastTouched();
+//    }
+
+    public void beginMorph(android.view.View v){
+        
+    }
 
     public void clearAll(android.view.View v){
-        CirclesDrawingView firstImg = findViewById(R.id.firstImage);
-        CirclesDrawingView secondImg = findViewById(R.id.secondImage);
         firstImg.clearLines();
         secondImg.clearLines();
     }
